af56e185159862b026188a5118666a001964a14a
Joelle <castelli.jo@gmail.com>
2021-04-17 16:44:20 +0200

    Models creation

---
+++ www/Models/Media.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\Database;
+
+class Media extends Database
+{
+
+    private $id = null;
+    protected $title;
+    protected $path;
+    protected $video = 0;
+    protected $createdAt;
+    protected $deletedAt;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @return null
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param null $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param mixed $path
+     */
+    public function setPath($path): void
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVideo(): int
+    {
+        return $this->video;
+    }
+
+    /**
+     * @param int $video
+     */
+    public function setVideo(int $video): void
+    {
+        $this->video = $video;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     */
+    public function setCreatedAt($createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDeletedAt()
+    {
+        return $this->deletedAt;
+    }
+
+    /**
+     * @param mixed $deletedAt
+     */
+    public function setDeletedAt($deletedAt): void
+    {
+        $this->deletedAt = $deletedAt;
+    }
+
+
+
+}
