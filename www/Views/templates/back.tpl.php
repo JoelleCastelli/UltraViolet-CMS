9f81a927f470449a4fff709c9ecc708b6e1453a1
Joelle <castelli.jo@gmail.com>
2021-04-22 19:06:11 +0200

    update back office page title

--- www/Views/templates/back.tpl.php
+++ www/Views/templates/back.tpl.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title>BACK</title>
+    <title>UltraViolet - <?= $title ?? ' Back office'?></title>
 	<meta name="description" content="description de la page de front">
 	<link rel="stylesheet" href="../../dist/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
