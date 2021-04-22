b2184f6c67136c02c87c1413dff047d9abe53f3a
Joelle <castelli.jo@gmail.com>
2021-04-22 20:23:23 +0200

    Comments: showAll action and list view

---
+++ www/Controllers/Comment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use App\Core\View;
+
+class Comment
+{
+
+    public function showAllAction() {
+        $view = new View("comments/list");
+        $view->assign('title', 'Commentaires');
+    }
+
+}
