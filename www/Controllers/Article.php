5df08e34b9abef423f8a809ad508a5ac09716099
Joelle <castelli.jo@gmail.com>
2021-04-22 20:22:51 +0200

    Articles: showAll action and list view

--- www/Controllers/Article.php
+++ www/Controllers/Article.php
@@ -5,7 +5,12 @@ namespace App\Controller;
 use App\Core\View;
 
 class Article {
-    
+
+    public function showAllAction() {
+        $view = new View("articles/list");
+        $view->assign('title', 'Articles');
+    }
+
     public function modifyArticleAction() {
         $view = new View("articles/modifyArticle", "back");
         $view->assign("title", "Modifier un article");
