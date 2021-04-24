b03e57f329d069bc2c75976b19e63d709988a2a3
Joelle <castelli.jo@gmail.com>
2021-04-24 02:21:34 +0200

    new FormValidator

--- www/Core/FormValidator.php
+++ www/Core/FormValidator.php
@@ -1,197 +1,139 @@
 <?php
+
 namespace App\Core;
 
 class FormValidator
 {
 
-    public static function check($config, $data)
-    {
+    public static function check($config, $data) {
 
         self::validateCSFRToken($config);
         $errors = [];
 
-        $checkboxes = isset($config["checkboxes"]) ? count($config["checkboxes"]) : 0;
-        $radios = isset($config["radios"]) ? count($config["radios"]) : 0;
-        $selects = isset($config["selects"]) ? count($config["selects"]) : 0;
-        $inputs = isset($config["inputs"]) ? count($config["inputs"]) : 0;
-        $total_inputs =  $inputs + $checkboxes + $radios + $selects;
-
-        // check if numbers of inputs is correct
-        if (count($data) < $config['config']['required_inputs'] || count($data) > $total_inputs)
-        {
+        if(count($data) != count($config["fields"])) {
             $errors[] = "Tentative de HACK - Faille XSS";
-        }
-        else {
+        } else {
+            foreach ($config["fields"] as $fieldName => $fieldConfig) {
 
-            foreach ($config["inputs"] as $name => $configInputs) {
-
-                $data[$name] = htmlspecialchars($data[$name], ENT_QUOTES);
-
-                // if form has not been changed
-                if(!isset($data[$name]))
-                {
-                    echo "Tentative de hack formulaire";
+                // check if config field has a matching $_POST field
+                if(!isset($data[$fieldName])) {
+                    echo "Tentative de hack !";
                     exit;
                 }
 
-                // if input is required or not empty
-                if( !empty($configInputs["required"]) || !empty($data[$name]))
-                {
-
-                    if($configInputs["type"] == "text")
-                        self::textValidator($data[$name], $configInputs, $errors);
-
-                    if($configInputs["type"] == "number")
-                        self::numberValidator($data[$name], $configInputs, $errors);
-
-                    //check password match
-                    if ($name == "pwd")
-                        self::passwordValidator($data[$name], $configInputs, $errors);
-
-                    //check password confirm
-                    if ($name == "pwdConfirm")
-                        self::passwordConfirmationValidator($data[$name], $data[$configInputs["confirm"]], $configInputs, $errors);
-
-                    // check email
-                    if ($configInputs["type"] == "email")
-                        self::emailValidator($data["email"], $configInputs, $errors);
-
-                    // check date min
-                    if($configInputs["type"] == "date")
-                        self::dateValidator($data[$name], "Y-m-d", $configInputs, $errors);
-
+                // check if required field is not empty
+                if (isset($fieldConfig['required']) && empty($data[$fieldName])) {
+                    echo "Tentative de hack : le champ est obligatoire !";
+                    exit;
                 }
 
+                if(!empty($data[$fieldName])) {
+                    self::textInputValidator($data[$fieldName], $fieldConfig, $errors);
+                    self::numberInputValidator($data[$fieldName], $fieldConfig, $errors);
+                    self::passwordValidator($fieldName, $data[$fieldName], $fieldConfig, $errors);
+                    self::passwordConfirmationValidator($fieldName, $data, $fieldConfig, $errors);
+                    self::emailInputValidator($data["email"], $fieldConfig, $errors);
+                    self::dateInputValidator($data[$fieldName], "Y-m-d", $fieldConfig, $errors);
+                    self::optionsValidator($data[$fieldName], $fieldConfig);
+                }
             }
+        }
+        return $errors;
+    }
 
-            if (isset($config["selects"] )) {
-                foreach ($config["selects"] as $name => $configSelects) {
+    public static function textInputValidator($textInput, $fieldConfig, &$errors) {
+        if($fieldConfig["type"] == "text") {
+            if (!empty($fieldConfig["minLength"]) && strlen($textInput) < $fieldConfig["minLength"]) {
+                $errors[] = $fieldConfig["error"];
+            }
+            if (!empty($fieldConfig["maxLength"]) && strlen($textInput) > $fieldConfig["maxLength"]) {
+                $errors[] = $fieldConfig["error"];
+            }
+            if (!empty($fieldConfig["regex"]) && !preg_match($fieldConfig["regex"], $textInput)) {
+                $errors[] = $fieldConfig["error"];
+            }
+        }
+    }
 
-                    if(!empty($configSelects["required"]) && (!isset($data[$name]) || empty($data[$name])))
-                    {
-                        $errors[] = $configSelects["error"];
+    public static function numberInputValidator($numberInput, $fieldConfig, &$errors){
+        if($fieldConfig["type"] == "number") {
+            if (!empty($field["min"]) && is_numeric($fieldConfig["min"]) && $numberInput < $fieldConfig["min"] ) {
+                $errors[] = $fieldConfig["error"];
+            }
+            if (!empty($field["max"]) && is_numeric($fieldConfig["max"]) && $numberInput > $field["max"]) {
+                $errors[] = $fieldConfig["error"];
+            }
+        }
+    }
+
+    public static function passwordValidator($fieldName, $password, $fieldConfig, &$errors) {
+        if ($fieldName == "pwd") {
+            if (!preg_match($fieldConfig["regex"], $password)  ) {
+                $errors[] = $fieldConfig["error"];
+            }
+        }
+    }
+
+    public static function passwordConfirmationValidator($fieldName, $data, $fieldConfig, &$errors) {
+        if ($fieldName == "pwdConfirm") {
+            if ($data['pwdConfirm'] !== $data[$fieldConfig["confirm"]]) {
+                $errors[] = $fieldConfig["error"];
+            }
+        }
+    }
+
+    public static function emailInputValidator($email, $fieldConfig, &$errors) {
+        if ($fieldConfig["type"] == "email") {
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $errors[] = $fieldConfig["error"];
+            }
+        }
+    }
+
+    public static function dateInputValidator($date, $format, $fieldConfig, &$errors) {
+        if($fieldConfig["type"] == "date") {
+            $d = \DateTime::createFromFormat($format, $date);
+            if($d && $d->format($format) == $date) {
+                if (!empty($fieldConfig["min"]) && (new \DateTime($date) <= new \DateTime($fieldConfig["min"]))) {
+                    $errors[] = $fieldConfig["error"];
+                }
+                if (!empty($fieldConfig["max"]) && (new \DateTime($date) >= new \DateTime($fieldConfig["max"]))) {
+                    $errors[] = $fieldConfig["error"];
+                }
+            }
+        }
+    }
+
+    public static function optionsValidator($fieldContent, $fieldConfig) {
+        $correctOptions = [];
+        if(in_array($fieldConfig["type"], ['select', 'radio'])) {
+            $correctOptions = [false];
+            foreach ($fieldConfig['options'] as $option) {
+                if ($fieldContent == $option['value']) {
+                    $correctOptions = [true];
+                }
+            }
+        } elseif($fieldConfig["type"] == 'checkbox') {
+            foreach ($fieldContent as $key => $value) {
+                $correctOptions[$key] = false;
+                foreach ($fieldConfig['options'] as $option) {
+                    if ($value == $option['value']) {
+                        $correctOptions[$key] = true;
                     }
                 }
             }
-
-            if (isset($config["radios"] )) {
-
-                foreach ($config["radios"] as $name => $configRadios) {
-
-                    if(!empty($configRadios["required"]) && (!isset($data[$name]) || empty($data[$name]))
-                    )
-                    {
-                        $errors[] = $configRadios["error"];
-                    }
-                }
-            }
-
-            if (isset($config["checkboxes"] )) {
-                foreach ($config["checkboxes"] as $name => $configCheckboxes) {
-
-                    if(!empty($configCheckboxes["required"]) && (!isset($data[$name]) || empty($data[$name])))
-                    {
-                        $errors[] = $configCheckboxes["error"];
-                    }
-
-                }
-            }
-
         }
-
-        return $errors; //[] empty if it's all okay
-    }
-
-    public function textValidator($text, $configInputs, &$errors)
-    {
-        // check string min length
-        if (!empty($configInputs["minLength"]) && strlen($text) < $configInputs["minLength"]) {
-            $errors[] = $configInputs["error"];
-            return;
-        }
-
-        // check string max length
-        if (!empty($configInputs["maxLength"]) && strlen($text) > $configInputs["maxLength"]) {
-            $errors[] = $configInputs["error"];
-            return;
-        }
-
-        // check string max length
-        if (!empty($configInputs["regex"]) && !preg_match($configInputs["regex"], $text)) {
-            $errors[] = $configInputs["error"];
-            return;
+        if(in_array(false, $correctOptions)) {
+            echo "Tentative de hack : l'option n'existe pas !";
+            exit;
         }
     }
 
-    public function numberValidator($number, $configInputs, &$errors)
-    {
-        // check number min length
-        if (!empty($configInputs["min"]) && is_numeric($configInputs["min"]) && $number < $configInputs["min"] ) {
-            $errors[] = $configInputs["error"];
-            return;
-        }
-
-        // check number max length
-        if (!empty($configInputs["max"]) && is_numeric($configInputs["max"]) && $number > $configInputs["max"]) {
-            $errors[] = $configInputs["error"];
-            return;
-        }
-    }
-
-    public function passwordValidator($password, $configInputs, &$errors) {
-        if (!preg_match($configInputs["regex"], $password)  ) {
-            $errors[] = $configInputs["error"];
-            return;
-        }
-    }
-
-    public function passwordConfirmationValidator($passwordConfirm, $password, $configInputs, &$errors) {
-        if ($passwordConfirm !== $password) {
-            $errors[] = $configInputs["error"];
-            return;
-        }
-    }
-
-    public function emailValidator($email, $configInputs, &$errors)
-    {
-        $pattern = "/^[a-zA-Z][\w\-\.]+@([\w-]+\.)+[\w]{2,4}$/";
-        if(!preg_match($pattern, $email))
-        {
-            $configInputs["error"] = "Votre email n'est pas valide";
-            $errors[] = $configInputs["error"];
-            return;
-        }
-
-    }
-
-    public function dateValidator($date, $format, $configInputs, &$errors)
-    {
-        $d = \DateTime::createFromFormat($format, $date); // create object date from format a date in string
-
-        if( $d && $d->format($format) == $date)
-        {
-            // check date min
-            if (!empty($configInputs["min"]) && (new \DateTime($date) <= new \DateTime($configInputs["min"]))) {
-                $errors[] = $configInputs["error"];
-                return;
-            }
-
-            // check date max
-            if (!empty($configInputs["max"]) && (new \DateTime($date) >= new \DateTime($configInputs["max"]))) {
-                $errors[] = $configInputs["error"];
-                return;
-
-            }
-        }
-    }
-
-    public static function getCSRFToken()
-    {
+    public static function getCSRFToken() {
         $message = '-encyclopedie Anticonstitutionnellement Ceci est une phrase random-';
         $hash = 'sha256';
 
-        if(empty($_SESSION['csrf_token']))
-        {
+        if(empty($_SESSION['csrf_token'])) {
             $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
             $_SESSION['csrf_token_time'] = time();
         }
@@ -202,45 +144,36 @@ class FormValidator
         return $token;
     }
 
-    public function validateCSFRToken($config){
-
+    public static function validateCSFRToken($config) {
 
         $message = '-encyclopedie Anticonstitutionnellement Ceci est une phrase random-';
         $hash = 'sha256';
 
-        if(isset($_SESSION['csrf_token']) && isset($_SESSION['csrf_token_time']) && isset($config['config']['csrf']))
-        {
+        if(isset($_SESSION['csrf_token']) && isset($_SESSION['csrf_token_time']) && isset($config['config']['csrf'])) {
 
             $token = hash_hmac($hash, $message,  $_SESSION['csrf_token']);
 
             // check if the session's token equals form's token
-            if(hash_equals($token, $config['config']['csrf']))
-            {
+            if(hash_equals($token, $config['config']['csrf'])) {
                 // timestamp of 15 minutes
                 $timestamp_ancien = time() - (15*60);
 
                 // check if the timestamp is expired
-                if($_SESSION['csrf_token_time'] >= $timestamp_ancien)
-                {
-
+                if($_SESSION['csrf_token_time'] >= $timestamp_ancien) {
                     unset($_SESSION['csrf_token_time']);
                     unset($_SESSION['csrf_token']);
-
-                }else{
+                } else {
                     echo "CSRF ATTACK time not correct";
                     die();
                 }
-            }else {
-
+            } else {
                 echo "CSRF ATTACK csrf not equal";
                 die();
             }
-        }else {
-
+        } else {
             echo "CSRF ATTACK csrf not set";
             die();
         }
-
     }
 
 }
