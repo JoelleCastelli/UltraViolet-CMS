1ff03928fd37c8ebde4a5dc2210bf7e4c0889e69
Coraline Esedji <cesedji@myges.fr>
2021-04-25 13:36:36 +0200

    add filter and tag in view list comment

--- www/Views/back/comments/list.view.php
+++ www/Views/back/comments/list.view.php
@@ -3,26 +3,30 @@
         <div class="grid-filter-list-comments">
 
             <!-- FILTER -->
-            <span class="label-card-1">Par article(s) : </span>
-            <div class="search-bar">
-                <i class="fas fa-search"></i>
-                <input type="text" placeholder="Taper un article...">
-            </div>
+            <article class="grid-filter-container-list-comments">
 
-            <span class="label-card-1">Depuis le : </span>
-            <div class="search-bar">
-                <i class="fas fa-search"></i>
-                <input type="text" placeholder="00/00/0000">
-            </div>
+                <span class="label-card-1">Par article(s) : </span>
+                <div class="search-bar">
+                    <i class="fas fa-search"></i>
+                    <input type="text" placeholder="Taper un article...">
+                </div>
 
-            <span class="label-card-1">Jusqu'au : </span>
-            <div class="search-bar">
-                <i class="fas fa-search"></i>
-                <input type="text" placeholder="00/00/0000">
-            </div>
+                <span class="label-card-1">Depuis le : </span>
+                <div class="search-bar">
+                    <i class="fas fa-search"></i>
+                    <input type="text" placeholder="00/00/0000">
+                </div>
+
+                <span class="label-card-1">Jusqu'au : </span>
+                <div class="search-bar">
+                    <i class="fas fa-search"></i>
+                    <input type="text" placeholder="00/00/0000">
+                </div>
+
+            </article>
 
             <!-- TAGS -->
-            <div class="filter-container">
+            <article class="grid-tag-container-list-comments">
                 <button class="filter-tag">
                     <span>Nom de l'oeuvre</span>
                     <i class="fas fa-times"></i>
@@ -35,7 +39,7 @@
                     <span>Oeuvre 3 avec un nom vachement plus long</span>
                     <i class="fas fa-times"></i>
                 </button>
-            </div>
+            </article>
 
         </div>
     </section>
