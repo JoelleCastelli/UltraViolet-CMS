5e03537558ef9583f87ee65cfe455f1f2d8802d2
Norudah <pierucci.romain@gmail.com>
2021-04-25 12:19:23 +0200

    Add filtering-status SCSS component

--- www/Views/back/articles/list.view.php
+++ www/Views/back/articles/list.view.php
@@ -1,70 +1,80 @@
 <div>
-    <div class="articleState" id="published">Publiés</div>
-    <div class="articleState" id="draft">Brouillons</div>
-    <div class="articleState" id="scheduled">Planifiés</div>
-    <div class="articleState" id="trash">Corbeille</div>
+
+    <div class="row-between">
+        <div class="filtering-status">
+            <div class="filtering-btn" id="published">Publiés</div>
+            <div class="filtering-btn" id="draft">Brouillons</div>
+            <div class="filtering-btn" id="scheduled">Planifiés</div>
+            <div class="filtering-btn" id="trash">Corbeille</div>
+        </div>
+
+        <button class="btn">Ajouter un article</button>
+    </div>
+
+
+
+
+    <table id="datatable" class="display">
+        <thead>
+        <tr>
+            <th>Colonne 1</th>
+            <th>Colonne 2</th>
+            <th>Colonne 3</th>
+            <th>Colonne 4</th>
+            <th>Colonne 5</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Option 1</td>
+            <td>Option 2</td>
+            <td>Option 3</td>
+            <td>Option 4</td>
+            <td>Option 5</td>
+        </tr>
+        <tr>
+            <td>Option 6</td>
+            <td>Option 7</td>
+            <td>Option 8</td>
+            <td>Option 9</td>
+            <td>Option 10</td>
+        </tr>
+        <tr>
+            <td>Option 6</td>
+            <td>Option 7</td>
+            <td>Option 8</td>
+            <td>Option 9</td>
+            <td>Option 10</td>
+        </tr>
+        <tr>
+            <td>Option 6</td>
+            <td>Option 7</td>
+            <td>Option 8</td>
+            <td>Option 9</td>
+            <td>Option 10</td>
+        </tr>
+        <tr>
+            <td>Option 6</td>
+            <td>Option 7</td>
+            <td>Option 8</td>
+            <td>Option 9</td>
+            <td>Option 10</td>
+        </tr>
+        <tr>
+            <td>Option 6</td>
+            <td>Option 7</td>
+            <td>Option 8</td>
+            <td>Option 9</td>
+            <td>Option 10</td>
+        </tr>
+        <tr>
+            <td>Option 6</td>
+            <td>Option 7</td>
+            <td>Option 8</td>
+            <td>Option 9</td>
+            <td>Option 10</td>
+        </tr>
+        </tbody>
+    </table>
+
 </div>
-
-
-<table id="datatable" class="display">
-    <thead>
-    <tr>
-        <th>Colonne 1</th>
-        <th>Colonne 2</th>
-        <th>Colonne 3</th>
-        <th>Colonne 4</th>
-        <th>Colonne 5</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-        <td>Option 1</td>
-        <td>Option 2</td>
-        <td>Option 3</td>
-        <td>Option 4</td>
-        <td>Option 5</td>
-    </tr>
-    <tr>
-        <td>Option 6</td>
-        <td>Option 7</td>
-        <td>Option 8</td>
-        <td>Option 9</td>
-        <td>Option 10</td>
-    </tr>
-    <tr>
-        <td>Option 6</td>
-        <td>Option 7</td>
-        <td>Option 8</td>
-        <td>Option 9</td>
-        <td>Option 10</td>
-    </tr>
-    <tr>
-        <td>Option 6</td>
-        <td>Option 7</td>
-        <td>Option 8</td>
-        <td>Option 9</td>
-        <td>Option 10</td>
-    </tr>
-    <tr>
-        <td>Option 6</td>
-        <td>Option 7</td>
-        <td>Option 8</td>
-        <td>Option 9</td>
-        <td>Option 10</td>
-    </tr>
-    <tr>
-        <td>Option 6</td>
-        <td>Option 7</td>
-        <td>Option 8</td>
-        <td>Option 9</td>
-        <td>Option 10</td>
-    </tr>
-    <tr>
-        <td>Option 6</td>
-        <td>Option 7</td>
-        <td>Option 8</td>
-        <td>Option 9</td>
-        <td>Option 10</td>
-    </tr>
-    </tbody>
-</table>
