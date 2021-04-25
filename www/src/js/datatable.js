01ee82abbad4e3f66ea45e62999edeb603c04f93
Norudah <pierucci.romain@gmail.com>
2021-04-25 21:40:42 +0200

    wrap datable footer + fix center layout for status switch + add somme styling to datatables

--- www/src/js/datatable.js
+++ www/src/js/datatable.js
@@ -1,3 +1,13 @@
+const wrapDatatablesFooter = () => {
+    const toAdd = document.querySelectorAll("#datatable_info, #datatable_paginate");
+    
+    const newFooter = document.createElement("div");
+    newFooter.classList.add("table-footer");
+
+    toAdd.forEach((element) => newFooter.appendChild(element));
+    document.querySelector('#datatable').after(newFooter);
+}
+
 $(document).ready( function () {
     $('#datatable').DataTable( {
         language: {
@@ -32,4 +42,5 @@ $(document).ready( function () {
     } );
     $("#datatable_filter label").before( "<i class='fas fa-search'>" );
     $(".dataTables_wrapper").children().slice(0,2).wrapAll( "<div id='tableHeader'></div>" );
+    wrapDatatablesFooter();
 } );
