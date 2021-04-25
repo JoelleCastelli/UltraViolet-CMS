2b5c3fd0c8917afd82ebd1b0187d2fb1295ee5ef
Joelle <castelli.jo@gmail.com>
2021-04-25 02:11:31 +0200

    articles list: get data from db

--- www/Controllers/Article.php
+++ www/Controllers/Article.php
@@ -8,9 +8,12 @@ use App\Models\Article as ArticleModel;
 class Article {
 
     public function showAllAction() {
+        $article = new ArticleModel;
+        $articles = $article->selectWhere('state', 'published');
         $view = new View("articles/list");
         $view->assign('title', 'Articles');
         $view->assign('headScript', 'src/js/headScripts/articles.js');
+        $view->assign('articles', $articles);
     }
 
     public function tabChangeAction() {
