367b88a908aa56e72f4f764e85d2041369754aaf
Coraline Esedji <cesedji@myges.fr>
2021-05-13 18:28:57 +0200

    add get all medias

--- www/Controllers/Media.php
+++ www/Controllers/Media.php
@@ -10,6 +10,19 @@ use App\Models\Media as MediaModels;
 class Media
 {
 
+    /**
+     * @var string[]
+     */
+    protected $columnsTable;
+
+    public function __construct() {
+        $this->columnsTable = [
+            "title" => 'Nom',
+            "path" => 'Chemin',
+            "video" => 'Vidéo',
+        ];
+    }
+
     public function showAllAction() {
         $view = new View("medias/list");
         $view->assign('title', 'Média');
@@ -17,8 +30,45 @@ class Media
         $media = new MediaModels();
         $form = $media->formBuilderUpload();
         $view->assign("form", $form);
-        $view->assign('bodyScript', 'src/js/bodyScripts/medias.js');
+        $view->assign('bodyScript', Helpers::urlJS('/bodyScripts/medias'));
 
+    }
+
+    public function getMediasAction() {
+
+        if(empty($_POST['mediaType'])) // get all medias
+        {
+            $media = new MediaModels();
+            $medias = $media->findAll();
+
+            if(!$medias) $medias = [];
+
+        }else { // get medias by type
+
+            echo 'oops';
+
+        }
+
+        // populate in arrays
+        $mediaArray = [];
+
+        foreach ($medias as $media) {
+
+            $mediaArray[] = array(
+                $this->columnsTable['title'] => $media->getTitle(),
+                $this->columnsTable['path'] => $media->getPath(),
+                $this->columnsTable['video'] => $media->getVideo(),
+            );
+        }
+
+        // send data
+        $data = array(
+            "medias" => $mediaArray,
+            "columns" => $this->columnsTable,
+            "type" => $_POST['mediaType']??""
+        );
+
+        echo json_encode($data);
     }
 
     public function uploadAction()
--- www/src/js/bodyScripts/medias.js
+++ www/src/js/bodyScripts/medias.js
@@ -1,3 +1,39 @@
 $(document).ready( function () {
-    console.log("hello");
+
+    // get all medias
+    $.ajax({
+        type: 'POST',
+        url: '/admin/medias/medias-data',
+        dataType: 'json',
+        success: function(response) {
+
+            console.log(response);
+            /*table.clear();
+            table.rows.add(response.productions).draw();*/
+        },
+        error: function(){
+            alert("error");
+        }
+    });
+
+    // get medias by type
+    $('.media-type').click(function () {
+
+        $.ajax({
+            type: 'POST',
+            url: '/admin/medias/medias-data',
+            dataType: 'json',
+            success: function(response) {
+
+                console.log(response);
+                /*table.clear();
+                table.rows.add(response.productions).draw();*/
+            },
+            error: function(){
+                alert("error");
+            }
+        });
+
+    });
+
 })
