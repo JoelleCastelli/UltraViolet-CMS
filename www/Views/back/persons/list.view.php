49c11420421e474e3b7cbc249b8fc90b1b2406bf
Sulyk <sboudacher@myges.fr>
2021-06-13 18:40:12 +0200

    Update view and person for the table

--- www/Views/back/persons/list.view.php
+++ www/Views/back/persons/list.view.php
@@ -4,6 +4,17 @@
 
 <div class="grid-listing-datatables">
 
+<section>
+        <div class="filtering-status">
+            <div class="filtering-btn active" id="user">Utilisateurs</div>
+            <div class="filtering-btn" id="moderator">Modérateurs</div>
+            <div class="filtering-btn" id="editor">Editeurs</div>
+            <div class="filtering-btn" id="admin">Adminstateurs</div>
+            
+            
+        </div>
+        <button class="btn">Ajouter une production</button>
+</section>
 
  <section>
      <table id="datatable" class="display">
