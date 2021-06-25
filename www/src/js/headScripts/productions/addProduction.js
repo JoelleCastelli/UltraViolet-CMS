e4d98816438924f659e3490b573833b6955c8ff0
Joelle <castelli.jo@gmail.com>
2021-06-25 19:31:18 +0200

    tmdb form: fix js

--- www/src/js/headScripts/productions/addProduction.js
+++ www/src/js/headScripts/productions/addProduction.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
+    // On page reload, reenable fields if series
+    if($('input[name="productionType"]:checked').val() === 'series') {
+        $("input[name='seasonNb']").prop('disabled', false);
+        if($('input[name="seasonNb"]').val() !== '') {
+            $("input[name='episodeNb']").prop('disabled', false);
+        }
+    }
+
     $("input[name='productionType']").change(function(){
         if($(this).val() === "movie") {
             $("input[name='seasonNb']").prop('disabled', true);
