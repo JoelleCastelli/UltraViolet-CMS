673b18baaca07c4d4936b62bb7a27956585ba509
Joelle <castelli.jo@gmail.com>
2021-07-03 15:04:18 +0200

    remove notification bell in header

--- www/Views/components/header.php
+++ www/Views/components/header.php
@@ -1,7 +1,6 @@
 <header class="header">
-    <h1><?= $title ?? 'Titre de la page'?></h1>
+    <h1><?= $title ?? ''?></h1>
     <div class=left-controls>
-        <i class="fas fa-bell"></i>
         <div class="user-label">
             <span><?= \App\Core\Request::getUser()->getPseudo() ?></span>
             <img src="<?= \App\Core\Request::getUser()->getMedia()->getPath() ?>" alt="Photo de profil">
