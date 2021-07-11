b1d75d83c0683cf74b900ce0e0b788bcb2f90d36
Norudah <pierucci.romain@gmail.com>
2021-07-11 10:57:35 +0200

    re-enable errors displaying of article update

--- www/Views/back/articles/updateArticle.view.php
+++ www/Views/back/articles/updateArticle.view.php
@@ -1,5 +1,17 @@
 <div>
 
+    <?php if (isset($errors)) {
+            echo "<div class='error-message-form'>";
+            foreach ($errors as $error) {
+                if (count($errors) == 1)
+                    echo "$error";
+                else
+                    echo "<li>$error</li>";
+            }
+            echo "</div>";
+        }
+    ?>
+
     <section class="grid-create-article">
 
         <section class="card">
