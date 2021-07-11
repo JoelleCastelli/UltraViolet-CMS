705fc596096782b7801ec26b9bc1674111a93da9
Joelle <castelli.jo@gmail.com>
2021-07-11 16:13:53 +0200

    database: update PDO to set charset to utf8

--- www/Core/Database.php
+++ www/Core/Database.php
@@ -17,16 +17,16 @@ class Database {
         if ($this->pdo === null) {
             if (ENV === "dev") {
                 try {
-                    $this->pdo = new \PDO(DBDRIVER . ":host=" . DBHOST . ";dbname=" . DBNAME . ";port=" . DBPORT, DBUSER, DBPWD);
+                    $this->pdo = new \PDO(DBDRIVER.":host=".DBHOST."; dbname=".DBNAME."; port=".DBPORT."; charset=UTF8", DBUSER, DBPWD);
                     $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                 } catch (Exception $e) {
-                    die("Erreur SQL : " . $e->getMessage());
+                    die("Erreur SQL (dev mode) : " . $e->getMessage());
                 }
             } else if (ENV === "prod") {
                 try {
-                    $this->pdo = new \PDO(DBDRIVER . ":host=" . DBHOST . ";dbname=" . DBNAME . ";port=" . DBPORT, DBUSER, DBPWD);
+                    $this->pdo = new \PDO(DBDRIVER.":host=".DBHOST."; dbname=".DBNAME."; port=".DBPORT."; charset=UTF8", DBUSER, DBPWD);
                 } catch (Exception $e) {
-                    die("Erreur connexion bdd côté production");
+                    die("La connexion à la base de données n'a pas pu être effectuée");
                 }
             }
         }
