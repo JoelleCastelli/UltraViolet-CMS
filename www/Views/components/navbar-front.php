ae52db6f9803f8bcb4852eddfcaa2160f2981bf9
Coraline <cesedji@myges.fr>
2021-07-12 11:29:13 +0200

    update navbar

--- www/Views/components/navbar-front.php
+++ www/Views/components/navbar-front.php
@@ -1,25 +1,26 @@
 <?php
 
+use App\Models\Category;
 use App\Core\Helpers;
 use App\Core\Request;
 
-$categories = Helpers::getCategories();
+$categories = Category::getMenuCategories();
 ?>
 
 <nav id="navbar-front">
-    <a href="<?= Helpers::callRoute('admin') ?>" class="brandLogo">
+    <a href="<?= Helpers::callRoute('front_home') ?>" class="brandLogo">
         <img src='<?= PATH_TO_IMG ?>logo_uv.png' alt='ultraviolet logo'>
     </a>
 
     <?php foreach ($categories['main'] as $mainCategory) : ?>
-        <a href="/categorie/<?= Helpers::slugify($mainCategory->getName()) ?>"><?= $mainCategory->getName() ?></a>
+        <a href="<?= Helpers::callRoute('display_category', ['category' => Helpers::slugify($mainCategory->getName())]) ?>"><?= $mainCategory->getName() ?></a>
     <?php endforeach; ?>
 
     <div class="dropdown">
         <span class="dropdown-button">Toutes les catégories</span>
         <div class="dropdown-content">
             <?php foreach ($categories['other']  as $otherCategory) : ?>
-                <a href="/categorie/<?= Helpers::slugify($otherCategory->getName()) ?>"><?= $otherCategory->getName() ?></a>
+                <a href="<?= Helpers::callRoute('display_category', ['category' => Helpers::slugify($otherCategory->getName())]) ?>"><?= $otherCategory->getName() ?></a>
             <?php endforeach; ?>
         </div>
     </div>
@@ -27,13 +28,20 @@ $categories = Helpers::getCategories();
     <?php
     $user = Request::getUser();
     if ($user && $user->isLogged()) : ?>
-        <a href="<?= Helpers::callRoute('logout') ?>"><button class="btn btn-login">Deconnexion</button></a>
-        <?php if ($user->canAccessBackOffice()) : ?>
-            <a href="<?= Helpers::callRoute('admin') ?>"><button class="btn btn-register">Administration</button></a>
-        <?php endif; ?>
+
+        <div class="dropdown">
+            <button class="dropdown-button"><img class="img-profile" src="<?= Request::getUser()->getMedia()->getPath() ?>"></button>
+            <div class="dropdown-content">
+                <a href="#">Paramètres</a>
+                <?php if ($user->canAccessBackOffice()) : ?>
+                    <a href="<?= Helpers::callRoute('admin') ?>">Administration</a>
+                <?php endif; ?>
+                <a href="<?= Helpers::callRoute('logout') ?>">Deconnexion</a>
+            </div>
+        </div>
 
     <?php else : ?>
-        <a href="<?= Helpers::callRoute('subscription') ?>"><button class="btn btn-register">S'inscrire</button></a>
+        <a href="<?= Helpers::callRoute('register') ?>"><button class="btn btn-register">S'inscrire</button></a>
         <a href="<?= Helpers::callRoute('login') ?>"><button class="btn btn-login">Connexion</button></a>
     <?php endif; ?>
 
