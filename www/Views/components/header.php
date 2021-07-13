a2a232dffcb8408e5275aff0b1dc7145ec8eaf99
Coraline <cesedji@myges.fr>
2021-07-13 16:04:13 +0200

    add dropdown in header (css deguelasse)

--- www/Views/components/header.php
+++ www/Views/components/header.php
@@ -1,9 +1,24 @@
+<?php
+
+use App\Core\Helpers;
+use App\Core\Request;
+?>
+
 <header class="header">
-    <h1><?= $title ?? ''?></h1>
+    <h1><?= $title ?? '' ?></h1>
     <div class=left-controls>
-        <div class="user-label">
-            <span><?= \App\Core\Request::getUser()->getPseudo() ?></span>
-            <img src="<?= \App\Core\Request::getUser()->getMedia()->getPath() ?>" alt="Photo de profil">
+        <div id='userImage' class="dropdown dropdown-button user-label">
+            <span><?= Request::getUser()->getPseudo() ?></span>
+            <img src="<?= Request::getUser()->getMedia()->getPath() ?>" alt="Photo de profil">
+            <div class="dropdown-content dropdown-user">
+                <a href="#">Paramètres</a>
+                <a href="<?= Helpers::callRoute('front_home') ?>"><?= APP_NAME ?></a>
+                <a href="<?= Helpers::callRoute('logout') ?>">Déconnexion</a>
+            </div>
         </div>
+        <!--   <div class="user-label">
+            <span><?= \App\Core\Request::getUser()->getPseudo() ?></span>
+            <img class="img-profil" src="<?= \App\Core\Request::getUser()->getMedia()->getPath() ?>" alt="Photo de profil">
+        </div> -->
     </div>
 </header>
