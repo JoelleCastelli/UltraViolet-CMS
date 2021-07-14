d189cbe586d912623284dd15d931f99d455527ec
Norudah <pierucci.romain@gmail.com>
2021-07-14 22:19:48 +0200

    Add style for article list

--- www/Views/front/articles/list.view.php
+++ www/Views/front/articles/list.view.php
@@ -2,23 +2,24 @@
 
 use App\Core\Helpers; ?>
 
+<h1>Tous les articles concernant : <strong><?= $category ?></strong></h1>
+
 <?php if (empty($articles)) : ?>
     <p>Aucun article associés à cette catégorie.</p>
 <?php else : ?>
-    <ul>
-
+    <section class="article-list">
         <?php foreach ($articles as $article) : ?>
-
-            <li>
-
-                Image : <img style="height: 6rem; width: 10rem" src="<?= $article->getMedia()->getPath(); ?>"><br>
-                Dernière MAJ : <?= $article->getContentUpdatedAt(); ?><br>
-                Titre : <a href="<?= Helpers::callRoute('display_article', ['article' => $article->getSlug()])  ?>"><?= $article->getTitle(); ?></a><br>
-                Description : <?= $article->getDescription(); ?><br>
-                Contenu : <?= $article->getContent(); ?><br>
-
-            </li>
-
+        <a href="<?= Helpers::callRoute('display_article', ['article' => $article->getSlug()])  ?>">
+            <article class="grid-article-card article-card">
+                <img class="article-card__cover" src="<?= $article->getMedia()->getPath(); ?>" alt="">
+                <h2 class="article-card__title"><?= $article->getTitle(); ?></h2>
+                <p class="article-card__description"><?= $article->getDescription(); ?></p>
+                <small class="article-card__author">
+                    <span>par <?=$article->getPerson()->getPseudo(); ?></span>
+                    <span>Publié le : <?=$article->getPublicationDate(); ?></span>
+                </small>
+            </article>
+        </a>
         <?php endforeach; ?>
-    </ul>
+    </section>
 <?php endif; ?>
