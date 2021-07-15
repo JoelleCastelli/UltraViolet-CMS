f58f662a422bb66231b531ab8aeb2dae070457e1
Boudacher Sylvain <sboudacher@myges.fr>
2021-07-15 12:19:24 +0200

    fix page password forget

--- www/Views/front/forgetPassword.view.php
+++ www/Views/front/forgetPassword.view.php
@@ -14,6 +14,7 @@
     <section class="grid-form-forget-password-user">
         <h1 class="title-form">UltraViolet</h1>
         <section class="card">
+            <h3> Entrer l'email de votre compte pour récupérer votre mot de passe</h3>
             <?php if (empty($send)) {
                 echo "<section class=\"container-form\">";
                 echo App\Core\FormBuilder::render($form, true);
