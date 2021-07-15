f2460231ab45546d2f31fd8c4c02f6d3e6ab0d2f
Coraline <cesedji@myges.fr>
2021-07-15 15:50:04 +0200

    update footer bien deguelasse

--- www/Views/components/footer-front.php
+++ www/Views/components/footer-front.php
@@ -1,7 +1,26 @@
-<?php use App\Core\Helpers; ?>
+<?php
 
-<footer id="footer-front" >
+use App\Core\Helpers; ?>
+
+
+<footer id="footer-front">
+
+    <article class="logo-section">
+        <a href="<?= Helpers::callRoute('front_home') ?>" class="brandLogo">
+            <img src='<?= PATH_TO_IMG ?>logo_uv.png' alt='ultraviolet logo'>
+        </a>
+    </article>
+
+    <article class="path-section">
+        <?php for ($i = 0; $i < 9; $i++) : ?>
+            <a href="#">Site <?= $i ?></a>
+        <?php endfor; ?>
+    </article>
+
+</footer>
+
+<!-- <footer id="footer-front">
     <?php foreach ($pages as $page) : ?>
         <a href="<?= Helpers::callRoute('display_page', ['page', $page->getSlug()]) ?>"><?= $page->getTitle() ?></a>
     <?php endforeach; ?>
-</footer>
+</footer> -->
