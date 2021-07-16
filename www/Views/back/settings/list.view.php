740aa928e568ebfd7117db805cecee7a59f8d5aa
Joelle <castelli.jo@gmail.com>
2021-07-16 15:30:08 +0200

    Settings: remove access to db info

--- www/Views/back/settings/list.view.php
+++ www/Views/back/settings/list.view.php
@@ -13,16 +13,5 @@
 ?>
 
 <div id="settings">
-
     <?php App\Core\FormBuilder::render($form) ?>
-
-    <div id="dbSettings" class="card">
-        <h2>Informations de la base de données</h2>
-        <div><strong>Nom :</strong>  <?= $settings['DBNAME']?></div>
-        <div><strong>Hôte :</strong>  <?= $settings['DBHOST']?></div>
-        <div><strong>Port :</strong>  <?= $settings['DBPORT']?></div>
-        <div><strong>Utilisateur :</strong>  <?= $settings['DBUSER']?></div>
-        <div><strong>Mot de passe :</strong> <?= $settings['DBPWD']?></div>
-        <div><strong>Driver :</strong> <?= $settings['DBDRIVER']?></div>
-    </div>
 </div>
