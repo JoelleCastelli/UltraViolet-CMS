e363c56c455df0ac95fb56b3f1ea371c9c1c78d7
Norudah <pierucci.romain@gmail.com>
2021-07-27 12:10:28 +0200

    Change timezone

--- www/Core/DatabaseConnection.php
+++ www/Core/DatabaseConnection.php
@@ -3,6 +3,7 @@
 namespace App\Core;
 
 use Exception;
+use DateTime;
 
 class DatabaseConnection {
 
@@ -10,12 +11,35 @@ class DatabaseConnection {
 
     private static $instance;
 
+    private function getOffset($timezone) : string {
+        
+        // Set PHP Timezone
+        date_default_timezone_set($timezone);
+
+        // Obtain the Offset
+        $time		= new DateTime();
+        $minutes	= $time->getOffset() / 60;
+        $sign		= ( $minutes < 0 ? -1 : 1 );
+        $minutes	= abs( $minutes );
+        $hours		= floor( $minutes / 60 );
+
+        $minutes -= $hours * 60;
+
+        $offset = sprintf( '%+d:%02d', $hours * $sign, $minutes );
+        return $offset;
+    }
+
     public function getConnection()
     {
         if ($this->pdo === null) {
+
+            $offset = $this->getOffset('Europe/Paris');
+
             if (ENV === "dev") {
                 try {
                     $this->pdo = new \PDO(DBDRIVER . ":host=" . DBHOST . "; dbname=" . DBNAME . "; port=" . DBPORT . "; charset=UTF8", DBUSER, DBPWD);
+                    // Set DB Timezone using the Offset
+	                $this->pdo->exec( "SET time_zone='$offset';" );
                     $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                 } catch (Exception $e) {
                     die("Erreur SQL (dev mode) : " . $e->getMessage());
@@ -23,6 +47,8 @@ class DatabaseConnection {
             } else if (ENV === "prod") {
                 try {
                     $this->pdo = new \PDO(DBDRIVER . ":host=" . DBHOST . "; dbname=" . DBNAME . "; port=" . DBPORT . "; charset=UTF8", DBUSER, DBPWD);
+                    // Set DB Timezone using the Offset
+	                $this->pdo->exec( "SET time_zone='$offset';" );
                 } catch (Exception $e) {
                     die("La connexion à la base de données n'a pas pu être effectuée");
                 }
