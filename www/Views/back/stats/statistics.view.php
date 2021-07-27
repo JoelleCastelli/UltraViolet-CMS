b699a132f9cdb42a17e90177a7fc6a7bd1a054e1
Boudacher Sylvain <sboudacher@myges.fr>
2021-07-27 16:17:53 +0200

    Fix statistic css

--- www/Views/back/stats/statistics.view.php
+++ www/Views/back/stats/statistics.view.php
@@ -9,87 +9,90 @@ if(isset($errors)) {
 ?>
 
 <div id="statistics">
+    <div class="row-stats">
 
-    <div class="card">
-        <div class="cardTitle">Statistiques du jour</div>
-        <div id="statToDay">
-            <div id='statToDayList'>
-                <div class='statToDayCard'>
-                    <span class="title">
-                        Articles publiés
-                    </span>
-                    <span class="stat">
-                        <?= $nbToDayArticles ?>
-                    </span>
-                </div>
-                <div class='statToDayCard'>
-                    <span class="title">
-                        Commentaires publiés
-                    </span>
-                    <span class="stat">
-                        <?= $nbToDayComments ?>
-                    </span>
-                </div>
-                <div class='statToDayCard'>
-                    <span class="title">
-                        Nouveaux utilisateurs
-                    </span>
-                    <span class="stat">
-                        <?= $nbToDayUsers ?>
-                    </span>
-                </div>
-                <div class='statToDayCard'>
-                    <span class="title">
-                        Vues totales
-                    </span>
-                    <span class="stat">
-                        <?= $nbToDayViews ?>
-                    </span>
+        <div class="card-statistique">
+            <div class="cardTitle">Statistiques du jour</div>
+            <div id="statToDay">
+                <div id='statToDayList'>
+                    <div class='statToDayCard'>
+                        <span class="title">
+                            Articles publiés
+                        </span>
+                        <span class="stat">
+                            <?= $nbToDayArticles ?>
+                        </span>
+                    </div>
+                    <div class='statToDayCard'>
+                        <span class="title">
+                            Commentaires publiés
+                        </span>
+                        <span class="stat">
+                            <?= $nbToDayComments ?>
+                        </span>
+                    </div>
+                    <div class='statToDayCard'>
+                        <span class="title">
+                            Nouveaux utilisateurs
+                        </span>
+                        <span class="stat">
+                            <?= $nbToDayUsers ?>
+                        </span>
+                    </div>
+                    <div class='statToDayCard'>
+                        <span class="title">
+                            Vues totales
+                        </span>
+                        <span class="stat">
+                            <?= $nbToDayViews ?>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    
+        
 
-    <div class="card">
-        <div class="cardTitle">Statistiques</div>
-        <div id="statistics">
-            <div id='statisticsList'>
-                <div class='statisticsCard'>
-                    <span class="title">
-                        Articles publiés
-                    </span>
-                    <span class="stat">
-                        <?= $nbArticles ?>
-                    </span>
-                </div>
-                <div class='statisticsCard'>
-                    <span class="title">
-                        Commentaires publiés
-                    </span>
-                    <span class="stat">
-                        <?= $nbComments ?>
-                    </span>
-                </div>
-                <div class='statisticsCard'>
-                    <span class="title">
-                        Utilisateurs
-                    </span>
-                    <span class="stat">
-                        <?= $nbUsers ?>
-                    </span>
-                </div>
-                <div class='statisticsCard'>
-                    <span class="title">
-                        Vues totales
-                    </span>
-                    <span class="stat">
-                        <?= $nbViews ?>
-                    </span>
+        <div class="card-statistique">
+            <div class="cardTitle">Statistiques</div>
+            <div id="statistics">
+                <div id='statisticsList'>
+                    <div class='statisticsCard'>
+                        <span class="title">
+                            Articles publiés
+                        </span>
+                        <span class="stat">
+                            <?= $nbArticles ?>
+                        </span>
+                    </div>
+                    <div class='statisticsCard'>
+                        <span class="title">
+                            Commentaires publiés
+                        </span>
+                        <span class="stat">
+                            <?= $nbComments ?>
+                        </span>
+                    </div>
+                    <div class='statisticsCard'>
+                        <span class="title">
+                            Utilisateurs
+                        </span>
+                        <span class="stat">
+                            <?= $nbUsers ?>
+                        </span>
+                    </div>
+                    <div class='statisticsCard'>
+                        <span class="title">
+                            Vues totales
+                        </span>
+                        <span class="stat">
+                            <?= $nbViews ?>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 
     <div class="card">
